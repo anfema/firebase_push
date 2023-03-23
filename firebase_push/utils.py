9ad8bbdef4a194fa12c0ab2ace03aab5a7e5a7c1
Johannes Schriewer <j.schriewer@anfe.ma>
2023-03-23 17:44:36 +0100

    Bugfix: Use same method as django to load model

--- firebase_push/utils.py
+++ firebase_push/utils.py
@@ -1,18 +1,32 @@
-from datetime import timedelta
-
+from django.apps import apps as django_apps
 from django.conf import settings
+from django.core.exceptions import ImproperlyConfigured
 from django.utils.module_loading import import_string
 
 
 def get_device_model():
-    app, model = settings.FCM_DEVICE_MODEL.split(".", maxsplit=1)
-    path = f"{app}.models.{model}"
-    FCMDevice = import_string(path)
-    return FCMDevice
+    """
+    Return the FCMDevice model that is active in this project.
+    """
+    try:
+        return django_apps.get_model(settings.FCM_DEVICE_MODEL, require_ready=False)
+    except ValueError:
+        raise ImproperlyConfigured("FCM_DEVICE_MODEL must be of the form 'app_label.model_name'")
+    except LookupError:
+        raise ImproperlyConfigured(
+            "FCM_DEVICE_MODEL refers to model '%s' that has not been installed" % settings.FCM_DEVICE_MODEL
+        )
 
 
 def get_history_model():
-    app, model = settings.FCM_PUSH_HISTORY_MODEL.split(".", maxsplit=1)
-    path = f"{app}.models.{model}"
-    FCMHistory = import_string(path)
-    return FCMHistory
+    """
+    Return the FCMHistory model that is active in this project.
+    """
+    try:
+        return django_apps.get_model(settings.FCM_PUSH_HISTORY_MODEL, require_ready=False)
+    except ValueError:
+        raise ImproperlyConfigured("FCM_PUSH_HISTORY_MODEL must be of the form 'app_label.model_name'")
+    except LookupError:
+        raise ImproperlyConfigured(
+            "FCM_PUSH_HISTORY_MODEL refers to model '%s' that has not been installed" % settings.FCM_PUSH_HISTORY_MODEL
+        )
